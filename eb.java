8329a5db3c6dbe95ad03e350b3ec495f42ae8061
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:23:54 +0000

    Create eb.java

---
+++ eb.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class eb
+{
+public static void main(String[] args)
+{
+int a,b,c,d,e;
+Scanner in=new Scanner(System.in);
+System.out.print("Enter Last Month Reading=");
+a=in.nextInt();
+System.out.print("Enter Current Month Reading=");
+b=in.nextInt();
+System.out.print("Enter Cost Per Unit=");
+e=in.nextInt();0
+c=b-a;
+d=c*e;
+System.out.println("No Of Units Is="+c);
+System.out.println("Total Amount="+d);
+}
+}
