7b1bc938ccb1caabb86569229cef9e5e0eb22b70
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:11:08 +0000

    Create twovar.java
    basic add a values using 2 variables

---
+++ twovar.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class twovar 
+{
+public static void main(String[] args)
+{
+int a,b;
+a=5;
+b=6;
+
+System.out.println("Add Value is"+(a+b));
+
+System.out.println("Sub Value is"+(a-b));
+
+System.out.println("Mul Value is"+(a*b));
+
+System.out.println("Div Value is"+(a/b));
+}
+} 
