e1c733ea44488d09103163a1b385f15bcd77b592
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:30:28 +0000

    Create binary.java

---
+++ binary.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class binary
+{
+public static void main(String[] args)
+{
+int a,b;
+int c;
+Scanner in=new Scanner(System.in);
+System.out.println("Enter a:");
+a=in.nextInt();
+System.out.println("Enter b:");
+b=in.nextInt();
+c=a&&b;
+System.out.println("The c is "+c);
+}
+}
