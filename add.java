a312bbb1fa9d7639c95b7a0f0ef0b9730f1fbf11
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:09:10 +0000

    Create add.java
    basic arthamatic oprater program

---
+++ add.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class add 
+{
+public static void main(String[] args)
+{
+int a,b,c;
+a=5;
+b=6;
+c=a-b;
+System.out.println("Subt Value is"+c);
+c=a+b;
+System.out.println("Add Value is"+c);
+c=a*b;
+System.out.println("Mul Value is"+c);
+c=a/b;
+System.out.println("Div Value is"+c);
+}
+}
