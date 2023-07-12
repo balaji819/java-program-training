68030d438fdf87a015228ad809cd350971522155
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:50:55 +0000

    Create tables.java

---
+++ tables.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class tables
+{
+public static void main(String[] args)
+{
+int i,j=16,k;
+for(i=1;i<=j;i++)
+{
+k=i*5;
+System.out.println(+i+"*5="+k);
+}
+}
+}
