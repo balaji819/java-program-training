5e0bf880e892a3e0003e6eff104d5853a39ef516
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:32:21 +0000

    Create shortcut.java

---
+++ shortcut.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class shortcut
+{
+public static void main(String[] args)
+{
+int a,b;
+a=5;
+b=6;
+a+=b;
+System.out.println("Add Value Is:"+a);
+a-=b;
+System.out.println("Sub Value Is:"+a);
+a*=b;
+System.out.println("Mul Value Is:"+a);
+a/=b;
+System.out.println("Div Value Is:"+a);
+a%=b;
+System.out.println("Mod Value Is:"+a);
+}
+}
