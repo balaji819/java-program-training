e86dba5ae476e7383df918b6d41bf99b958fe747
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:03:45 +0000

    Create bala.java
    welcome program

---
+++ bala.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class bala
+{
+public static void main(String[] args)
+{
+System.out.println("\n\t Welcome");
+System.out.println("\n\t To");
+System.out.println("\n\t JAVA");
+}
+}
