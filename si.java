3872481469b8bf17f2c55e75c18125b84e2a28a6
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:16:39 +0000

    Create si.java
    simple interest program

---
+++ si.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class si
+{
+public static void main(String[] args)
+{
+int p,n,r,si;
+p=5000;
+n=3;
+r=20;
+si=(p*n*r)/100;
+System.out.println("Simple Interest="+si);
+}
+}
