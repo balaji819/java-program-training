f78de7f17609b7fe2ebaa3010febd6e9e79b5f3c
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:14:58 +0000

    Create tot.java
    basic find the avg program

---
+++ tot.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class tot
+{
+public static void main(String[] args)
+{
+int m1,m2,m3,m4,m5;
+float avg,tot;
+m1=90;
+m2=100;
+m3=43;
+m4=85;
+m5=75;
+tot=m1+m2+m3+m4+m5;
+avg=tot/5;
+System.out.println("Mark1="+m1);
+System.out.println("Mark2="+m2);
+System.out.println("Mark3="+m3);
+System.out.println("Mark4="+m4);
+System.out.println("Mark5="+m5);
+System.out.println("Total="+tot);
+System.out.println("Average="+avg);
+}
+}
