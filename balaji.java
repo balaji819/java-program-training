18160a0d021d04362c8613a15c40c42443678572
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:50:15 +0000

    Create balaji.java

---
+++ balaji.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class balaji
+{
+public static void main(String[] args)
+{
+int i,n;
+Scanner in=new Scanner(System.in);
+System.out.print("Enter No Of Times To Print:");
+n=in.nextInt();
+for(i=1;i<=n;i++)
+{
+System.out.println("Balaji");
+}
+}
+}
