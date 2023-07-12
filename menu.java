408153f81be504256861dc90c9c4768fd375df90
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:07:08 +0000

    Create menu.java
    hottal menu program

---
+++ menu.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class menu
+{
+public static void main(String[] args)
+{
+System.out.println("----------------------------------------------------");
+System.out.println("S.no \t\tProduct \t\tRate");
+System.out.println("----------------------------------------------------");
+System.out.println("101 \t\tPongal \t\t40");
+System.out.println("102 \t\tDosa \t\t30");
+System.out.println("103 \t\tIdly \t\t7");
+}
+}
