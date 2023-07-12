b059ef1938454812ffde65b34465bac7d5f23a79
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:12:37 +0000

    Create swap.java
    basic swapping program

---
+++ swap.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class swap
+{
+public static void main(String[] args)
+{
+int a,b,c;
+a=5;
+b=6;
+c=a;
+a=b;
+b=c;
+System.out.println("a="+a);
+System.out.println("b="+b);
+}
+}
