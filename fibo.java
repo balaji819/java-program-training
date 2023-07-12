22d7bf1f8bf81928908a57623d10385d3d001d08
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-12 14:53:03 +0000

    Create fibo.java

---
+++ fibo.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class fibo
+{
+public static void main(String[] args)
+{
+int n1=0,n2=1,n3,count,i;
+Scanner in=new Scanner(System.in);
+System.out.print("Enter a Number:");
+count=in.nextInt();
+for(i=1;i<=count;i++)
+{
+n3=n1+n2;
+System.out.print("\n"+n1);
+n1=n2;
+n2=n3;
+}
+}
+}
