813b6da2acca9470da9615ff45a8c31e383e9c74
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-13 13:29:04 +0000

    Create pattern2.java

---
+++ pattern2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class pattern2
+{
+public static void main(String[] args)
+{
+int i,j,sum=0;
+for(i=1;i<=5;i++)
+{
+sum++;
+for(j=1;j<=i;j++)
+{
+System.out.print(" "+sum+" ");
+}
+System.out.println("");
+}
+}
+}
