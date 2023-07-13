12f1a41dbfc72854c854b2719668a1eb12a1422f
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-13 13:31:34 +0000

    Create pattern5.java

---
+++ pattern5.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class pattern5
+{
+public static void main(String[] args)
+{
+int i,j,b=1;
+for(i=1;i<=5;i++)
+{
+for(j=1;j<=i;j++)
+{
+
+System.out.print(" "+b+" ");
+b=b+2;
+}
+System.out.println("");
+}
+}
+}
