e2e565e40b055e26875a8f10d5f613a58534fadf
balaji <87461495+balaji819@users.noreply.github.com>
2023-07-13 13:29:55 +0000

    Create pattern3.java

---
+++ pattern3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class pattern3
+{
+public static void main(String[] args)
+{
+int i,j,b=0;
+for(i=1;i<=5;i++)
+{
+for(j=1;j<=i;j++)
+{
+b++;
+System.out.print(" "+b+" ");
+}
+System.out.println("");
+}
+}
+}
